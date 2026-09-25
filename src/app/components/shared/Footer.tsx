85dc024a6cb55f4ea24cb1f510ef97503acefa7f
Mahfuz Subed <mahfuzsubed@gmail.com>
2026-09-25 06:19:10 +0600

    added footer

---
+++ src/app/components/shared/Footer.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import logo from "@/assets/logo.png"
+const Footer = () => {
+    return (
+        <div className="flex justify-between items-center px-[2%]">
+            <div className="flex justify-center items-center gap-2">
+                <Image src={logo} alt="footer logo"></Image>
+                <h2 className="font-bold text-xl">FITLOG</h2>
+            </div>
+            <p>
+                &copy 2026 FitLog — Workout Library. Train hard, log honest.
+            </p>
+        </div> 
+    );
+};
+
+export default Footer;
